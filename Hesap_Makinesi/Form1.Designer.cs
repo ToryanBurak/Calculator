0775ef7793166501cd23709633f21b82d6445477
ToryanBurak <buraktoryan@gmail.com>
2023-01-18 12:46:42 +0300

    Program Files

---
+++ Hesap_Makinesi/Form1.Designer.cs
@@ -0,0 +1,344 @@
+﻿
+namespace Hesap_Makinesi
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.bir_button = new System.Windows.Forms.Button();
+            this.iki_button = new System.Windows.Forms.Button();
+            this.uc_button = new System.Windows.Forms.Button();
+            this.dort_button = new System.Windows.Forms.Button();
+            this.bes_button = new System.Windows.Forms.Button();
+            this.alti_button = new System.Windows.Forms.Button();
+            this.yedi_button = new System.Windows.Forms.Button();
+            this.sekiz_button = new System.Windows.Forms.Button();
+            this.dokuz_button = new System.Windows.Forms.Button();
+            this.sifir_button = new System.Windows.Forms.Button();
+            this.sifirlama_button = new System.Windows.Forms.Button();
+            this.toplam_button = new System.Windows.Forms.Button();
+            this.cikarma_button = new System.Windows.Forms.Button();
+            this.bolum_button = new System.Windows.Forms.Button();
+            this.carpim_button = new System.Windows.Forms.Button();
+            this.sonuc_button = new System.Windows.Forms.Button();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.sonuc_label = new System.Windows.Forms.Label();
+            this.sayi2_label = new System.Windows.Forms.Label();
+            this.isaret_label = new System.Windows.Forms.Label();
+            this.sayi1_label = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // bir_button
+            // 
+            this.bir_button.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.bir_button.Location = new System.Drawing.Point(12, 108);
+            this.bir_button.Name = "bir_button";
+            this.bir_button.Size = new System.Drawing.Size(71, 69);
+            this.bir_button.TabIndex = 0;
+            this.bir_button.Text = "&1";
+            this.bir_button.UseVisualStyleBackColor = true;
+            this.bir_button.Click += new System.EventHandler(this.bir_button_Click);
+            // 
+            // iki_button
+            // 
+            this.iki_button.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.iki_button.Location = new System.Drawing.Point(86, 108);
+            this.iki_button.Name = "iki_button";
+            this.iki_button.Size = new System.Drawing.Size(71, 69);
+            this.iki_button.TabIndex = 1;
+            this.iki_button.Text = "&2";
+            this.iki_button.UseVisualStyleBackColor = true;
+            this.iki_button.Click += new System.EventHandler(this.iki_button_Click);
+            // 
+            // uc_button
+            // 
+            this.uc_button.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.uc_button.Location = new System.Drawing.Point(160, 108);
+            this.uc_button.Name = "uc_button";
+            this.uc_button.Size = new System.Drawing.Size(71, 69);
+            this.uc_button.TabIndex = 2;
+            this.uc_button.Text = "&3";
+            this.uc_button.UseVisualStyleBackColor = true;
+            this.uc_button.Click += new System.EventHandler(this.uc_button_Click);
+            // 
+            // dort_button
+            // 
+            this.dort_button.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.dort_button.Location = new System.Drawing.Point(12, 183);
+            this.dort_button.Name = "dort_button";
+            this.dort_button.Size = new System.Drawing.Size(71, 69);
+            this.dort_button.TabIndex = 3;
+            this.dort_button.Text = "&4";
+            this.dort_button.UseVisualStyleBackColor = true;
+            this.dort_button.Click += new System.EventHandler(this.dort_button_Click);
+            // 
+            // bes_button
+            // 
+            this.bes_button.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.bes_button.Location = new System.Drawing.Point(83, 183);
+            this.bes_button.Name = "bes_button";
+            this.bes_button.Size = new System.Drawing.Size(71, 69);
+            this.bes_button.TabIndex = 4;
+            this.bes_button.Text = "&5";
+            this.bes_button.UseVisualStyleBackColor = true;
+            this.bes_button.Click += new System.EventHandler(this.bes_button_Click);
+            // 
+            // alti_button
+            // 
+            this.alti_button.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.alti_button.Location = new System.Drawing.Point(160, 183);
+            this.alti_button.Name = "alti_button";
+            this.alti_button.Size = new System.Drawing.Size(71, 69);
+            this.alti_button.TabIndex = 5;
+            this.alti_button.Text = "&6";
+            this.alti_button.UseVisualStyleBackColor = true;
+            this.alti_button.Click += new System.EventHandler(this.alti_button_Click);
+            // 
+            // yedi_button
+            // 
+            this.yedi_button.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.yedi_button.Location = new System.Drawing.Point(12, 258);
+            this.yedi_button.Name = "yedi_button";
+            this.yedi_button.Size = new System.Drawing.Size(71, 69);
+            this.yedi_button.TabIndex = 6;
+            this.yedi_button.Text = "&7";
+            this.yedi_button.UseVisualStyleBackColor = true;
+            this.yedi_button.Click += new System.EventHandler(this.yedi_button_Click);
+            // 
+            // sekiz_button
+            // 
+            this.sekiz_button.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.sekiz_button.Location = new System.Drawing.Point(86, 258);
+            this.sekiz_button.Name = "sekiz_button";
+            this.sekiz_button.Size = new System.Drawing.Size(71, 69);
+            this.sekiz_button.TabIndex = 7;
+            this.sekiz_button.Text = "&8";
+            this.sekiz_button.UseVisualStyleBackColor = true;
+            this.sekiz_button.Click += new System.EventHandler(this.sekiz_button_Click);
+            // 
+            // dokuz_button
+            // 
+            this.dokuz_button.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.dokuz_button.Location = new System.Drawing.Point(160, 258);
+            this.dokuz_button.Name = "dokuz_button";
+            this.dokuz_button.Size = new System.Drawing.Size(71, 69);
+            this.dokuz_button.TabIndex = 8;
+            this.dokuz_button.Text = "&9";
+            this.dokuz_button.UseVisualStyleBackColor = true;
+            this.dokuz_button.Click += new System.EventHandler(this.dokuz_button_Click);
+            // 
+            // sifir_button
+            // 
+            this.sifir_button.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.sifir_button.Location = new System.Drawing.Point(86, 333);
+            this.sifir_button.Name = "sifir_button";
+            this.sifir_button.Size = new System.Drawing.Size(71, 69);
+            this.sifir_button.TabIndex = 9;
+            this.sifir_button.Text = "&0";
+            this.sifir_button.UseVisualStyleBackColor = true;
+            this.sifir_button.Click += new System.EventHandler(this.sifir_button_Click);
+            // 
+            // sifirlama_button
+            // 
+            this.sifirlama_button.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.sifirlama_button.Location = new System.Drawing.Point(12, 333);
+            this.sifirlama_button.Name = "sifirlama_button";
+            this.sifirlama_button.Size = new System.Drawing.Size(71, 65);
+            this.sifirlama_button.TabIndex = 11;
+            this.sifirlama_button.Text = "&C";
+            this.sifirlama_button.UseVisualStyleBackColor = true;
+            this.sifirlama_button.Click += new System.EventHandler(this.sifirlama_button_Click);
+            // 
+            // toplam_button
+            // 
+            this.toplam_button.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.toplam_button.Location = new System.Drawing.Point(237, 258);
+            this.toplam_button.Name = "toplam_button";
+            this.toplam_button.Size = new System.Drawing.Size(71, 69);
+            this.toplam_button.TabIndex = 12;
+            this.toplam_button.Text = "&+";
+            this.toplam_button.UseVisualStyleBackColor = true;
+            this.toplam_button.Click += new System.EventHandler(this.toplam_button_Click);
+            // 
+            // cikarma_button
+            // 
+            this.cikarma_button.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.cikarma_button.Location = new System.Drawing.Point(237, 333);
+            this.cikarma_button.Name = "cikarma_button";
+            this.cikarma_button.Size = new System.Drawing.Size(71, 69);
+            this.cikarma_button.TabIndex = 13;
+            this.cikarma_button.Text = "&-";
+            this.cikarma_button.UseVisualStyleBackColor = true;
+            this.cikarma_button.Click += new System.EventHandler(this.cikarma_button_Click);
+            // 
+            // bolum_button
+            // 
+            this.bolum_button.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.bolum_button.Location = new System.Drawing.Point(237, 183);
+            this.bolum_button.Name = "bolum_button";
+            this.bolum_button.Size = new System.Drawing.Size(71, 69);
+            this.bolum_button.TabIndex = 14;
+            this.bolum_button.Text = "%";
+            this.bolum_button.UseVisualStyleBackColor = true;
+            this.bolum_button.Click += new System.EventHandler(this.bolum_button_Click);
+            // 
+            // carpim_button
+            // 
+            this.carpim_button.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.carpim_button.Location = new System.Drawing.Point(237, 108);
+            this.carpim_button.Name = "carpim_button";
+            this.carpim_button.Size = new System.Drawing.Size(71, 69);
+            this.carpim_button.TabIndex = 15;
+            this.carpim_button.Text = "X";
+            this.carpim_button.UseVisualStyleBackColor = true;
+            this.carpim_button.Click += new System.EventHandler(this.carpim_button_Click);
+            // 
+            // sonuc_button
+            // 
+            this.sonuc_button.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.sonuc_button.Location = new System.Drawing.Point(160, 333);
+            this.sonuc_button.Name = "sonuc_button";
+            this.sonuc_button.Size = new System.Drawing.Size(71, 69);
+            this.sonuc_button.TabIndex = 16;
+            this.sonuc_button.Text = "=";
+            this.sonuc_button.UseVisualStyleBackColor = true;
+            this.sonuc_button.Click += new System.EventHandler(this.sonuc_button_Click);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+            this.pictureBox4.Location = new System.Drawing.Point(12, 2);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(296, 102);
+            this.pictureBox4.TabIndex = 20;
+            this.pictureBox4.TabStop = false;
+            // 
+            // sonuc_label
+            // 
+            this.sonuc_label.AutoSize = true;
+            this.sonuc_label.BackColor = System.Drawing.Color.White;
+            this.sonuc_label.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.sonuc_label.Location = new System.Drawing.Point(26, 63);
+            this.sonuc_label.Name = "sonuc_label";
+            this.sonuc_label.Size = new System.Drawing.Size(0, 21);
+            this.sonuc_label.TabIndex = 23;
+            // 
+            // sayi2_label
+            // 
+            this.sayi2_label.AutoSize = true;
+            this.sayi2_label.BackColor = System.Drawing.Color.White;
+            this.sayi2_label.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.sayi2_label.Location = new System.Drawing.Point(98, 9);
+            this.sayi2_label.Name = "sayi2_label";
+            this.sayi2_label.Size = new System.Drawing.Size(0, 21);
+            this.sayi2_label.TabIndex = 21;
+            // 
+            // isaret_label
+            // 
+            this.isaret_label.AutoSize = true;
+            this.isaret_label.BackColor = System.Drawing.Color.White;
+            this.isaret_label.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.isaret_label.Location = new System.Drawing.Point(62, 9);
+            this.isaret_label.Name = "isaret_label";
+            this.isaret_label.Size = new System.Drawing.Size(0, 21);
+            this.isaret_label.TabIndex = 22;
+            // 
+            // sayi1_label
+            // 
+            this.sayi1_label.AutoSize = true;
+            this.sayi1_label.BackColor = System.Drawing.Color.White;
+            this.sayi1_label.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.sayi1_label.Location = new System.Drawing.Point(26, 9);
+            this.sayi1_label.Name = "sayi1_label";
+            this.sayi1_label.Size = new System.Drawing.Size(0, 21);
+            this.sayi1_label.TabIndex = 24;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.sonuc_button;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.sifirlama_button;
+            this.ClientSize = new System.Drawing.Size(316, 410);
+            this.Controls.Add(this.sayi1_label);
+            this.Controls.Add(this.sonuc_label);
+            this.Controls.Add(this.isaret_label);
+            this.Controls.Add(this.sayi2_label);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.sonuc_button);
+            this.Controls.Add(this.carpim_button);
+            this.Controls.Add(this.bolum_button);
+            this.Controls.Add(this.cikarma_button);
+            this.Controls.Add(this.toplam_button);
+            this.Controls.Add(this.sifirlama_button);
+            this.Controls.Add(this.sifir_button);
+            this.Controls.Add(this.dokuz_button);
+            this.Controls.Add(this.sekiz_button);
+            this.Controls.Add(this.yedi_button);
+            this.Controls.Add(this.alti_button);
+            this.Controls.Add(this.bes_button);
+            this.Controls.Add(this.dort_button);
+            this.Controls.Add(this.uc_button);
+            this.Controls.Add(this.iki_button);
+            this.Controls.Add(this.bir_button);
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "HESAP MAKİNESİ";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        public System.Windows.Forms.Button bir_button;
+        public System.Windows.Forms.Button iki_button;
+        public System.Windows.Forms.Button uc_button;
+        public System.Windows.Forms.Button alti_button;
+        public System.Windows.Forms.Button bolum_button;
+        public System.Windows.Forms.Button carpim_button;
+        public System.Windows.Forms.Button dort_button;
+        public System.Windows.Forms.Button bes_button;
+        public System.Windows.Forms.Button yedi_button;
+        public System.Windows.Forms.Button sekiz_button;
+        public System.Windows.Forms.Button dokuz_button;
+        public System.Windows.Forms.Button sifir_button;
+        public System.Windows.Forms.Button sifirlama_button;
+        public System.Windows.Forms.Button toplam_button;
+        public System.Windows.Forms.Button cikarma_button;
+        public System.Windows.Forms.Button sonuc_button;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Label sonuc_label;
+        private System.Windows.Forms.Label sayi2_label;
+        private System.Windows.Forms.Label isaret_label;
+        private System.Windows.Forms.Label sayi1_label;
+    }
+}
+
